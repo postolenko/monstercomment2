a487871873e4bf1428dd30eb283960940b035af6
postolenko <denis.postolenko@gmail.com>
2018-08-28 16:04:21 +0300

    Monstercomment

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,72 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
    
+    $(".show_popup").click(function(e) {
 
+        e.preventDefault();
+
+        popupName = $(this).attr("data-popup-name");
+        popupBlock = $("[data-popup = '"+ popupName +"']");
+
+        popupBlock.fadeIn(400);
+
+    });
+
+     $(this).keydown(function(eventObject){
+
+        if (eventObject.which == 27) {
+
+            if ( $(".popup_wrapp").is(":visible") ) {
+
+                $(".popup_wrapp").fadeOut(300);
+
+            }
+
+        }
+
+    });
+
+    $(".close-popup").click(function() {
+
+        popupBlock = $(this).closest(".popup_wrapp");
+
+        popupBlock.fadeOut(300);
+
+    });
+
+
+    $(document).mouseup(function (e){
+
+        hide_element = $('.popup');
+
+        if (!hide_element.is(e.target)
+
+            && hide_element.has(e.target).length === 0) {
+
+            hide_element.closest(".popup_wrapp").fadeOut(300);
+        }
+
+    });
+
+    $(".show_pass").click(function(e) {
+
+        e.preventDefault();
+
+        parentBlock = $(this).closest(".input_wrapp");
+
+        var passInput = parentBlock.find("input");
+
+        if(passInput.attr("type") == "password") {
+
+            passInput.attr("type", "text");
+
+        } else {
+
+            passInput.attr("type", "password");
+
+        }
+
+    });
 
 });
 
